6f9dc971e80ffcde5f46c4e7c663dbb9d1db8dd4
Debora Campelo <deboracampelo1978@gmail.com>
2024-07-09 11:47:41 -0300

    Criação da opção para Imprimir a Lista de Contratos

--- src/app/contract-grid/contract-grid.component.ts
+++ src/app/contract-grid/contract-grid.component.ts
@@ -36,4 +36,19 @@ export class ContractGridComponent implements OnInit {
       this.contratos = this.contratos.filter(c => c._id !== contrat._id);
     });
   }
+
+  printContratos() {
+    const printContent = document.getElementsByTagName('table')[0].outerHTML;
+    const printWindow = window.open('', '', 'height=600,width=800');
+    if (printWindow) {
+      printWindow.document.write('<html><head><title>Imprimir Lista de Contratos</title>');
+      printWindow.document.write('<style>th, td { text-align: center; }</style>');
+      printWindow.document.write('</head><body>');
+      printWindow.document.write(printContent);
+      printWindow.document.write('</body></html>');
+      printWindow.document.close();
+      printWindow.print();
+    }
+  }
+
 }
